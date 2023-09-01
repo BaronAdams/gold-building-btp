69eabd6ea26c8e147e5aa26c4560bb56f9c2488e
BaronAdams <rogeradamskouyangtaptoussia@gmail.com>
2023-09-01 04:34:22 +0100

    Others changes

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -11,24 +11,24 @@ async function connect() {
     //@ts-ignore
   if (db) return db;
 
-  // if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     //@ts-ignore
     db = await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       dbName:'gold-building-btp'
     });
-  // } else {
+  } else {
     // in development, need to store the db connection in a global variable
     // this is because the dev server purges the require cache on every request
     // and will cause multiple connections to be made
     //@ts-ignore
-    // if (!global.__db) {
-    //     //@ts-ignore
-    //   global.__db = await mongoose.connect('mongodb://127.0.0.1:27017/goldbuilding');
-    // }
+    if (!global.__db) {
+        //@ts-ignore
+      global.__db = await mongoose.connect('mongodb://127.0.0.1:27017/goldbuilding');
+    }
     //@ts-ignore
     db = global.__db;
-  // }
+  }
   return db;
 }
 
